3f47c5fcaee08ad6160eb3819b3b5653ebc5dbe0
Ravikiran Pathade <pathaderavi@gmail.com>
2018-01-15 18:48:54 -0500

    feat: Audio Recording Code Added

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -7,17 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    @IBOutlet weak var recordButtonVar: UIButton!
     
+    
+    @IBOutlet weak var recordButtonVar: UIButton!
     @IBOutlet weak var stopButtonVar: UIButton!
     @IBOutlet weak var statusLabel: UILabel!
+    var audioRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -34,7 +38,32 @@ class ViewController: UIViewController {
     @IBAction func stopButton(_ sender: Any) {
         stopButtonVar.isEnabled = false
         recordButtonVar.isEnabled = true
-         statusLabel.text = "Tap to Record"
+        statusLabel.text = "Tap to Record"
+        if(audioRecorder != nil){
+            audioRecorder.stop()
+            
+        }
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        statusLabel.text = "Recording in progress"
+        stopButtonVar.isEnabled = true
+        recordButtonVar.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
 }
 
