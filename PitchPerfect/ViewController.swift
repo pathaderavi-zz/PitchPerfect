724ddeab0629627208ab00689a5bd3cab35ce548
Ravikiran Pathade <pathaderavi@gmail.com>
2018-01-15 18:20:29 -0500

    feat: Buttons working

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordButtonVar: UIButton!
+    
+    @IBOutlet weak var stopButtonVar: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        stopButtonVar.isEnabled = false
+    }
+    @IBAction func recordButton(_ sender: Any) {
+        stopButtonVar.isEnabled = true
+        recordButtonVar.isEnabled = false
+        statusLabel.text = "Reording in Progress"
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        stopButtonVar.isEnabled = false
+        recordButtonVar.isEnabled = true
+         statusLabel.text = "Tap to Record"
+    }
 }
 
